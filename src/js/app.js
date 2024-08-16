2635cc94003ab77528aefb8a65e8c0e1033dec08
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-08-16 21:10:56 +0000

    finished

--- src/js/app.js
+++ src/js/app.js
@@ -68,7 +68,7 @@ function render(variables = {}) {
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.instagram}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://linkedin.com/school/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
